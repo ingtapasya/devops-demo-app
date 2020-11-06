c9be75d48d83fae24cbb25d3b768a060c47fdb8d
Roopa Yabaluri <ingroopa@gmail.com>
2020-11-05 18:10:57 -0800

    Fixed error message issue with modified syntax
    Fixed error message issue with modified syntax :
    ERROR 1193 (HY000): Unknown system variable 'validate_password_length'

--- devops-demo.sql
+++ devops-demo.sql
@@ -1,4 +1,9 @@
 CREATE TABLE dbversion (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,version VARCHAR(30) NOT NULL);
 INSERT INTO dbversion (version) VALUES ('1.0');
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' identified by 'devpass';
+SHOW VARIABLES LIKE 'validate_password%';
+SET GLOBAL validate_password_length = 6;
+SET GLOBAL validate_password_number_count = 0;
+SET GLOBAL validate_password.policy = LOW;
+CREATE USER 'devops_user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON devopsdb TO 'devops_user'@'%';
 flush PRIVILEGES;
